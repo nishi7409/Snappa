d2f21378aac2a6c60544830770413a726e8fc1fb
Nishant Srivastava <nishi7409@gmail.com>
2021-10-11 12:08:24 -0400

    home

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,21 +1,50 @@
+
+
 <template>
   <div class="home">
     <v-container>
-    <v-row>
-      <h1>Main home page</h1>
-    </v-row>    
-  </v-container>
+      <v-row>
+        <div class="hero">
+          <video playsinline autoplay muted loop>
+            <source :src='require("../assets/Home.mp4")' type='video/mp4'>
+          </video>
+          <div class="overlay"></div>
+        </div>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// // @ is an alias to /src
-// import home from '@/components/home.vue'
-
-// export default {
-//   name: 'Home',
-//   components: {
-//     home
-//   }
-// }
+  export default {
+    name: 'Home',
+    components: {
+    }
+  }
 </script>
+
+<style scoped>
+
+  video {
+    object-fit: cover !important;
+    width: 100vw;
+    height: 50%;
+    position: fixed;
+    left: 0;
+  }
+  
+  .hero .overlay {
+    width: 100%;
+    height: 50%;
+    position: absolute;
+    left: 0;
+    z-index: 2;
+    background: black;
+    opacity: 0.05;
+  }
+
+  .heroHeader {
+    color: white;
+  }
+
+</style>
