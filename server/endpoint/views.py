4454c4ebef269e84231dda89603bb470bac8464e
Nishant Srivastava <nishi7409@gmail.com>
2021-10-18 15:15:24 -0400

    view

--- server/endpoint/views.py
+++ server/endpoint/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 
 # Create your views here.
 class Logout(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         # simply delete the token to force a login
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
