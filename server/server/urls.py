eee726eacc5c19eadb8293fdd21dc5a5d01fa73c
Nishant Srivastava <nishi7409@gmail.com>
2021-11-06 19:08:10 -0400

    update

--- server/server/urls.py
+++ server/server/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    path('swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('', include('endpoint.urls')),
 ]
