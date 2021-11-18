ed6eba5242e0a8c0fb8ad31cc608a5078d1c173e
Sooryakumar Pari <paris@rpi.edu>
2021-11-17 19:07:59 -0500

    Commenting on views.py

--- server/endpoint/views.py
+++ server/endpoint/views.py
@@ -15,8 +15,10 @@ class GenerateUserObject(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
+            #checks to see if user already exisits
             if (len(User.objects.filter(username=str(request.data['username']))) == 1):
                 return Response(data={"response": False, "error": "There was already a saved User object for this user"})
+            #otherwise creates a new user
             elif (len(User.objects.filter(username=str(request.data['username']))) == 0):
                 user = User(username=str(request.data['username']), email=str(request.data['email']), stat1=0, stat2=0, stat3=0, stat4=0, stat5=0, stat6=0, stat7=0, stat8=0, stat9=0, stat10=0, stat11=0)
                 user.save()
@@ -25,15 +27,11 @@ class GenerateUserObject(APIView):
                 return Response(data={"response": True, "error": "THIS SHOULDN'T APPEAR"})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-<<<<<<< Updated upstream
-# Return user stats to the profile page
-=======
 # GenerateUserStats sends all stats of a user to the API endpoint under the URL getUserStats/ in JSON format
 # Inputs:
 #   User
 # Outputs:
 #   stats of the user
->>>>>>> Stashed changes
 class GenerateUserStats(APIView):
     def post(self, request, format=None):
         # serializer checks if the passed in data (json object) meets the desired requirements
@@ -42,7 +40,7 @@ class GenerateUserStats(APIView):
         if serializer.is_valid():
             if (len(User.objects.filter(username = tmpName)) == 1):
                 tmpUser = User.objects.get(username = tmpName)
-                
+                #returns the stats of the user (shots, table hits, points, clinks, dunks, potential points, catches, drops, table hit percentage, potential point percentage)
                 return Response(data={  "stat1" : tmpUser.stat1,
                                         "stat2" : tmpUser.stat2,
                                         "stat3" : tmpUser.stat3,
@@ -65,10 +63,13 @@ class LeagueCreate(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = LeagueCreateSerializer(data=request.data)
         if serializer.is_valid():
+            #check if league has already been started
             if (len(League.objects.filter(ownerUsername=str(request.data['ownerUsername']))) >= 1):
                 return Response(data={"response": False, "error": "User already started a league"})
+            #Checks to see if a unique league name was used
             elif (len(League.objects.filter(leagueName=str(request.data['leagueName']))) >= 1):
                 return Response(data={"response": False, "error": "League name has been used previously"})
+            #creates league
             else:
                 league = League(ownerUsername=str(request.data['ownerUsername']), leagueName=str(request.data['leagueName']), started=0, teamLength=int(request.data['teamLength']))
                 league.save()
@@ -81,9 +82,11 @@ class LeagueAddUser(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = LeagueAddUserSerializer(data=request.data)
         if serializer.is_valid():
+            #checks for invalid username
             if (len(League.objects.filter(ownerUsername=request.data['ownerUsername'])) == 0):
                 return Response(data={"response": False, "error": "League owner username is invalid"})
             else:
+                #adds user to the league league
                 currentLeague = League.objects.get(ownerUsername=request.data['ownerUsername'])
                 newUser = User.objects.get(username=request.data['username'])
                 # we're just going to assume user wasn't added previously
@@ -98,9 +101,11 @@ class GetActiveLeagueUsers(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = LeagueGetActiveUsersSerializer(data=request.data)
         if serializer.is_valid():
+            #checks for valid league name
             if (len(League.objects.filter(leagueName=request.data['leagueName'])) == 0):
                 return Response(data={"response": False, "error": "The data for the requested league doesn't exist"})
             else:
+                #returns all users of the league
                 allUsers = []
                 for x in League.objects.get(leagueName=request.data['leagueName']).allUsers.all():
                     allUsers.append(x.username)
@@ -113,8 +118,10 @@ class DoesLeagueExist(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = DoesLeagueExistSerializer(data=request.data)
         if serializer.is_valid():
+            #checks if the user is a league owner
             if (len(League.objects.filter(ownerUsername=request.data['username'])) == 0):
                 return Response(data={"response": False, "error": "User doesn't own any leagues"})
+            #checks how many teams are in the league and returns the league name and the number of teams
             allTeams = []
             for x in League.objects.get(ownerUsername=request.data['username']).allTeams.all():
                 allTeams.append(x)
@@ -131,12 +138,16 @@ class SubmitLeague(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = SubmitLeagueSerializer(data=request.data)
         if serializer.is_valid():
+            #checks for user initialization of league
             if (len(League.objects.filter(ownerUsername=request.data['username'])) == 0):
                 return Response(data={"response": False, "error": "User has not initialized a league yet"})
+            #checks if user is the league owner
             elif (League.objects.get(leagueName=request.data['leagueName']).ownerUsername != request.data['username']):
                 return Response(data={"response": False, "error": "You are not owner of this league"})
+            #makes sure that league is not already in play
             elif (League.objects.get(ownerUsername=request.data['username']).started == 1):
                 return Response(data={"response": False, "error": "League has already been started"})
+            #starts the league 
             currentLeague = League.objects.get(ownerUsername=request.data['username'])
             currentLeague.started = 1
             currentLeague.save()
@@ -149,8 +160,10 @@ class DeleteLeague(APIView):
         # serializer checks if the passed in data (json object) meets the desired requirements
         serializer = DeleteLeagueSerializer(data=request.data)
         if serializer.is_valid():
+            #checks to see if user owns the league
             if (len(League.objects.filter(ownerUsername=request.data['username'])) == 0):
                 return Response(data={"response": False, "error": "User doesn't own any leagues"})
+            #Deletes the league
             League.objects.filter(ownerUsername=request.data['username']).delete()
             return Response(data={"response": True, "error": "Deleted league owned by user"})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
